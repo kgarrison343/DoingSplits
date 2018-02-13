22a490f7084becfde484e9f0f3d057f69a71ee67
Kyle Garrison <kgarrison@nremt.org>
2018-02-13 13:35:15 -0500

    Add intent and onClick handler for items.

--- app/src/main/java/kgarrison/doingsplits/GamesListActivity.java
+++ app/src/main/java/kgarrison/doingsplits/GamesListActivity.java
@@ -1,7 +1,10 @@
 package kgarrison.doingsplits;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,5 +32,18 @@ public class GamesListActivity extends AppCompatActivity {
         );
 
         lv.setAdapter(gameAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                openSplit(view);
+            }
+        });
+    }
+
+    public void openSplit(View view){
+        Intent intent = new Intent(this, SplitActivity.class);
+
+        startActivity(intent);
     }
 }
