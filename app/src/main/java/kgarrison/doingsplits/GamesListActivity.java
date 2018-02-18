162fdd268e3d31e40d199872d2fb9725e0bb94c7
Kyle Garrison <kgarrison343@gmail.com>
2018-02-17 21:36:34 -0500

    Implement toolbar with button and custom icon

--- app/src/main/java/kgarrison/doingsplits/GamesListActivity.java
+++ app/src/main/java/kgarrison/doingsplits/GamesListActivity.java
@@ -1,8 +1,13 @@
 package kgarrison.doingsplits;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,13 +24,16 @@ public class GamesListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_list);
+        Toolbar actionBar = (Toolbar) findViewById(R.id.toolbar2);
+        this.setSupportActionBar(actionBar);
+        actionBar.setTitleTextColor(Color.WHITE);
 
         ListView lv = (ListView) findViewById(R.id.games);
-        List<Game> games = new ArrayList<Game>();
+        List<Game> games = new ArrayList<>();
         games.add(new Game("Greendog: The Beached Surfer Dude!", "Sega Genesis"));
         games.add(new Game("VVVVVV", "PC"));
 
-        ArrayAdapter<Game> gameAdapter = new ArrayAdapter<Game>(
+        ArrayAdapter<Game> gameAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 games
@@ -41,9 +49,31 @@ public class GamesListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_add:
+                getNewGame();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void openSplit(View view){
         Intent intent = new Intent(this, SplitActivity.class);
 
         startActivity(intent);
+    }
+
+    public void getNewGame(){
+
     }
 }
